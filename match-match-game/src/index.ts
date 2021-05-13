9bc0f2c5644e6931af436bdb9c9ba33f5e455370
A.Markelychev <a.markelychev@gmail.com>
2021-05-13 23:41:34 +0300

    feat: add class gameField

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,5 +1,7 @@
 import App from './components/app/app'
+import GameField from './components/gameField/gameField'
 import './styles.scss'
 
-
-const app = new App()
+const game = document.querySelector('.game') ?? document.body;
+const gameField = new GameField();
+game.append(gameField.element)
