8e58bc3f9767822f26cb3f8e93a554e7a09b22a7
A.Markelychev <a.markelychev@gmail.com>
2021-05-17 00:28:15 +0300

    fix: add style in header

--- match-match-game/src/components/header/header.ts
+++ match-match-game/src/components/header/header.ts
@@ -1,8 +1,24 @@
 import ElementCreator from '../elementCreator/elementCreator'
-import './header.scss'
+import './header.scss';
+
+const logoHTML = `<a href="" class="logo">
+      <span class="logo__w logo__w-top">match</span>
+      <span class="logo__w logo__w-bottom">match</span>
+    </a>`
 
 export default class Header extends ElementCreator {
   constructor() {
-    super('header', ['header'])
+    super('header', ['header']);
+    this.element.innerHTML = `${logoHTML}
+    <nav class="nav header__nav">
+      <ul class="nav__list">
+        <li class="nav__item nav__item-active">
+          <a href="" class="nav__link">About game</a>
+        </li>
+        <li class="nav__item"><a href="" class="nav__link">Game score</a></li>
+        <li class="nav__item"><a href="" class="nav__link">Settings</a></li>
+      </ul>
+    </nav>
+    <button class="btn">register new player</button>`
   }
 }
