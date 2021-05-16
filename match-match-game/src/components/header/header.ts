fc0450ab2a82e01d0278d875e8766e967d5495cd
A.Markelychev <a.markelychev@gmail.com>
2021-05-17 00:06:54 +0300

    feat: add header component

---
+++ match-match-game/src/components/header/header.ts
@@ -0,0 +1,8 @@
+import ElementCreator from '../elementCreator/elementCreator'
+import './headet.scss'
+
+export default class Header extends ElementCreator {
+  constructor() {
+    super('header', ['header'])
+  }
+}
