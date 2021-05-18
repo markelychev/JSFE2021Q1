4a4962e90616fe19b71f09ceb9806235f978c69b
A.Markelychev <a.markelychev@gmail.com>
2021-05-19 00:09:58 +0300

    feat: add mainNode and method render main

--- match-match-game/src/components/app/app.ts
+++ match-match-game/src/components/app/app.ts
@@ -1,10 +1,14 @@
+import Game from "../game/game";
 import Header from "../header/header"
 import observer from "../observer/observer";
 
 import Router from '../router/easyRouter';
 
+const imgs = ['img/eagle.jpg', 'img/elephant.jpg', 'img/lion.jpg', 'img/tiger.jpg', 'img/wolf.jpg'];
+
 export default class App {
-  private readonly header: Header = new Header()
+  private readonly header: Header = new Header();
+  private readonly mainNode = this.header.createElement('main', ['main']); //это вообще законно???
   constructor(private readonly rootNode: HTMLBaseElement | null) {
     this.addRouter()
   }
@@ -14,13 +18,22 @@ export default class App {
 
     router
       .addRoute('About_game', () => {
-        observer.notify('router', 'About_game')
+        observer.notify('router', 'About_game');
+        const test = this.header.createElement('p')
+        test.innerText = 'About_game';
+        this.renderMainContent(test)
       })
       .addRoute('Game_score', () => {
         observer.notify('router', 'Game_score')
+        const test = this.header.createElement('p')
+        test.innerText = 'Game_score';
+        this.renderMainContent(test)
       })
       .addRoute('Settings', () => {
         observer.notify('router', 'Settings')
+        const test = this.header.createElement('p')
+        test.innerText = 'Settings';
+        this.renderMainContent(test)
       })
       .addRoute('', () => {
         // general controller
@@ -29,9 +42,15 @@ export default class App {
 
   }
 
+  private renderMainContent = (content: HTMLElement) => {
+    this.mainNode.innerHTML = '';
+    this.mainNode.append(content);
+  }
+
   render = () => {
     if (this.rootNode) {
-      this.rootNode.prepend(this.header.element)
+      this.rootNode.prepend(this.mainNode);
+      this.rootNode.prepend(this.header.element);
     }
   }
 
