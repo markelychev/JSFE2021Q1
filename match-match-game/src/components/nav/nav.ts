59a16e72a90c40264a233ee72a3361e6d5e39e04
A.Markelychev <a.markelychev@gmail.com>
2021-05-18 23:39:51 +0300

    feat: add add and remove active class methods

--- match-match-game/src/components/nav/nav.ts
+++ match-match-game/src/components/nav/nav.ts
@@ -6,19 +6,31 @@ interface linkNode extends HTMLElement {
   href?: string
 }
 
+const activeClass = 'nav__link-active'
+
 const baseNavItem = ['About_game', 'Game_score', 'Settings'];
 
 export default class Nav extends ElementCreator {
-  private readonly list: HTMLElement
+  private readonly list: HTMLElement;
+  private readonly linkNodes: Map<string, HTMLElement> = new Map();
   constructor(private navItem: string[] = baseNavItem) {
     super('nav', ['nav', 'header__nav'])
     this.list = this.createList();
     this.element.append(this.list);
-    observer.subscribe('router', this.addActive)
+    observer.subscribe('router', this.addActiveClass)
   }
 
-  addActive = (data: string) => {
-    console.log(data)
+  private addActiveClass = (data: string) => {
+
+    const activeLinkNode = this.linkNodes.get(data);
+    if (activeLinkNode) {
+      this.removeActiveClass()
+      activeLinkNode.classList.add(activeClass)
+    }
+  }
+
+  private removeActiveClass = () => {
+    this.linkNodes.forEach(node => node.classList.remove(activeClass));
   }
 
   private createList = () => {
@@ -28,6 +40,7 @@ export default class Nav extends ElementCreator {
       const aNode: linkNode = this.createElement('a', ['nav__link'])
       aNode.href = '/#/' + item;
       aNode.innerText = item;
+      this.linkNodes.set(item, aNode);
       const li = this.createElement('li', ['nav__item']);
       li.append(aNode)
       return li;
