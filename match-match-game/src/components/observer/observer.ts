5059cdc09d09c3e4f0b72c577a0f00d336fbd617
A.Markelychev <a.markelychev@gmail.com>
2021-05-18 22:21:11 +0300

    feat: add observer

---
+++ match-match-game/src/components/observer/observer.ts
@@ -0,0 +1,19 @@
+class Observer {
+  private listeners: Map<string, Function[]> = new Map()
+  constructor() {
+  }
+
+  subscribe = (event: string, callback: Function) => {
+    let calbacks = this.listeners.get(event) ?? [];
+    calbacks.push(callback);
+    this.listeners.set(event, calbacks)
+  }
+
+  notify = (event: string, data: string) => {
+    this.listeners.get(event)?.forEach(callback => callback(data))
+  }
+}
+
+const observer = new Observer();
+
+export default observer;
