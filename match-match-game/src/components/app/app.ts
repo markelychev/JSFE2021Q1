a777593e6e4fecc1a7afd4e0c134b0b20e93121c
A.Markelychev <a.markelychev@gmail.com>
2021-05-20 23:35:59 +0300

    feat: add f-n clear to app, add render about component

--- match-match-game/src/components/app/app.ts
+++ match-match-game/src/components/app/app.ts
@@ -1,3 +1,4 @@
+import About from '../about/about.c';
 import Game from '../game/game';
 import Header from '../header/header';
 import observer from '../observer/observer';
@@ -8,34 +9,31 @@ const imgs = ['img/eagle.jpg', 'img/elephant.jpg', 'img/lion.jpg', 'img/tiger.jp
 
 export default class App {
   private readonly header: Header = new Header();
-
   private readonly mainNode = this.header.createElement('main', ['main']); // это вообще законно???
+  private readonly about = new About();
 
   constructor(private readonly rootNode: HTMLBaseElement | null) {
     this.addRouter();
   }
 
-  addRouter = () => {
+  private addRouter = () => {
     const router = new Router();
 
     router
       .addRoute('About_game', () => {
         observer.notify('router', 'About_game');
-        const test = this.header.createElement('p');
-        test.innerText = 'About_game';
-        this.renderMainContent(test);
+        this.clearMainContent();
+        this.about.render(this.mainNode);
       })
       .addRoute('Game_score', () => {
         observer.notify('router', 'Game_score');
         const test = this.header.createElement('p');
         test.innerText = 'Game_score';
-        this.renderMainContent(test);
       })
       .addRoute('Settings', () => {
         observer.notify('router', 'Settings');
         const test = this.header.createElement('p');
         test.innerText = 'Settings';
-        this.renderMainContent(test);
       })
       .addRoute('', () => {
         // general controller
@@ -43,9 +41,8 @@ export default class App {
       });
   };
 
-  private renderMainContent = (content: HTMLElement) => {
+  private clearMainContent = () => {
     this.mainNode.innerHTML = '';
-    this.mainNode.append(content);
   };
 
   render = () => {
